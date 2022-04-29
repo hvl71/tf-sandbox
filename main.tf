35438db77346bbff371e61305de0e5ada812e539
DevOpsGuyCph <cloud.devops.dk@gmail.com>
2022-04-29 22:35:40 +0200

    Moved verify-azure-access files to dedicated folder

--- main.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {}
-
-#mrg = my resource group - must not exist already according to above page
-resource "azurerm_resource_group" "temp01" {
-  name     = "SomeName01"
-  location = "East US"
-}
